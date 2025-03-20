72d63a778d72ee032e22fbf9098b57a153bb87df
nnaimas <naimasisreal@protonmail.com>
2025-03-20 08:28:55 +0700

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+function App() {
+  const [yourName, setName] = useState("");
+  return (
+    <div>
+      <label>Name: </label>
+      <input
+        type="text"
+        value={yourName}
+        onChange={(event) => setName(event.target.value)}
+      />
+
+      <h1>Hello {yourName} !</h1>
+    </div>
+  );
+}
