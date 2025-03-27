dafc3ff6a408e2121070f3130876a6d83be13ad3
Nhài Phạm <naimasisreal@protonmail.com>
2025-03-27 01:34:49 +0000

    oki

--- src/form3.js
+++ src/form3.js
@@ -14,11 +14,17 @@ export default function Form3() {
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <label>Your Name</label>
-      <input type="text" {...register("name", { required: "Name is required" })} />
+      <input
+        type="text"
+        {...register("name", { required: "Name is required" })}
+      />
       {errors.name && <p>{errors.name.message}</p>}
 
       <label>Password</label>
-      <input type="password" {...register("password", { required: "Password is required" })} />
+      <input
+        type="password"
+        {...register("password", { required: "Password is required" })}
+      />
       {errors.password && <p>{errors.password.message}</p>}
 
       <button type="submit">Submit</button>
